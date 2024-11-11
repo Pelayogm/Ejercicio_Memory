1b3063ca78e8d12463a9f502806dc919a6316e5a
Danitinhg <dani111hg@gmail.com>
2024-11-12 00:36:07 +0100

    v0.2.4 Creación de Tablero.css

--- src/Tablero.js
+++ src/Tablero.js
@@ -4,15 +4,32 @@ function Tablero () {
 
     const mapaCartas = [{   
           "type": "Charizard",
-          "image": "./img/cartas/Charizard-Tera.png"} ];
+          "image": "./img/cartas/Charizard-Tera.png", 
+        },
+        {
+          "type": "Lapras",
+          "image": "./img/cartas/Lapras-Vmax.png",
+        }
+        ];
     
 
     return (
         <div className="tablero">
-          <div id = "row"  className = 'row'>
-            <Carta></Carta>
+          <div className = "board">
+            <div id = "row" className = 'row'>
+              <div row = "1" colunm = "1"><Carta></Carta></div>
+              <div row = "1" colunm = "2"><Carta></Carta></div>
+              <div row = "1" colunm = "3"><Carta></Carta></div>
+              <div row = "1" colunm = "4"><Carta></Carta></div>
+            </div>
+            <div id = "row" className = 'row'>
+              <div row = "2" colunm = "1"><Carta></Carta></div>
+              <div row = "2" colunm = "2"><Carta></Carta></div>
+              <div row = "2" colunm = "3"><Carta></Carta></div>
+              <div row = "2" colunm = "4"><Carta></Carta></div>
+            </div>
           </div>
         </div>
-    )
+    );
 }
 export default Tablero;
