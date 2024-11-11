848a6a6f7a3fd6688985a1ace719100f9c3ea73e
Pelayogm <pelagrmenendez12@gmail.com>
2024-11-11 23:12:33 +0100

    Cambios en carta.js

--- src/Carta.js
+++ src/Carta.js
@@ -1,14 +1,22 @@
 import React from "react";
 import parte_trasera from "./img/cartas/Carta-atras.png";
+import parte_delantera from "./img/cartas/Lapras-Vmax.png";
 
 const Carta = ({ onClick, card, index, isInactive, isFlipped, isDisabled }) => {
-  const handleClick = () => {
-    !isFlipped && !isDisabled && onClick(index);
-  };
 
+  function cambiarImagen() {
+    const imagen = document.getElementById("carta");
+
+
+    if (imagen.src.includes({parte_trasera})) {
+        imagen.src = {parte_delantera};
+    } else {
+        imagen.src = "imagen1.jpg";
+    }
+}
   return (
-    <div onClick={handleClick}>
-      <img src="./img/cartas/Charizard-Tera.png"></img>
+    <div>
+      <img src={parte_trasera} onClick={cambiarImagen} id="carta" />
     </div>
   );
 };
