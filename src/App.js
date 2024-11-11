b7ffabfbdd0e83c512dc1e75e5b31eb219e38a66
Pelayogm <pelagrmenendez12@gmail.com>
2024-11-11 20:53:31 +0100

    v0.1.3 Comienzo con el tablero

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Tablero from './Tablero';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           <p>Prueba</p>
         </p>
+        <Tablero></Tablero>
       </header>
     </div>
   );
