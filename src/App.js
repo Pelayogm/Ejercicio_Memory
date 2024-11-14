e16d2f12199577eadec3e414be0db3d124f73094
Pelayogm <pelagrmenendez12@gmail.com>
2024-11-14 02:15:53 +0100

    v0.7.9 - Cambios en css e introducido el helmet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Helmet } from 'react-helmet';
 import Tablero from './Tablero';
 import luigi from './luigi';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Memory Game | Pelayogm & Danielma</title>
+      </Helmet>
       <body>
         <div className='App'>
           <p className='titulo-juego'>Juego Memory</p>
